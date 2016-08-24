a2bd9e217a78c5593a43c8657c2744c631976c88
Thomas Dufour <thomas.dufour@epitech.eu>
2016-08-24 16:06:20 +0200

    RecursiveTailRecM[DecodeResult]

--- argonaut-cats/src/main/scala/argonaut/DecodeResultCats.scala
+++ argonaut-cats/src/main/scala/argonaut/DecodeResultCats.scala
@@ -4,6 +4,7 @@ import CursorHistoryCats._
 import cats._, data._
 import ext.std.tuple._
 import instances.either._, instances.string._
+import scala.annotation.tailrec
 import syntax.contravariant._
 
 object DecodeResultCats extends DecodeResultCatss {
@@ -28,13 +29,20 @@ trait DecodeResultCatss {
   implicit def DecodeResultEq[A](implicit EA: Eq[A]): Eq[DecodeResult[A]] =
     catsStdEqForEither[(String, CursorHistory), A].on(_.toEither)
 
-  implicit def DecodeResultMonad: Monad[DecodeResult] = new Monad[DecodeResult] {
-    def pure[A](a: A) = DecodeResult.ok(a)
-    def flatMap[A, B](a: DecodeResult[A])(f: A => DecodeResult[B]) = a flatMap f
-    override def map[A, B](a: DecodeResult[A])(f: A => B) = a map f
-    override def tailRecM[A, B](a: A)(fn : A => DecodeResult[Either[A, B]]): DecodeResult[B] =
-      defaultTailRecM(a)(fn)
-  }
+  implicit def DecodeResultMonad: Monad[DecodeResult] with RecursiveTailRecM[DecodeResult] =
+    new Monad[DecodeResult] with RecursiveTailRecM[DecodeResult] {
+      def pure[A](a: A) = DecodeResult.ok(a)
+      def flatMap[A, B](a: DecodeResult[A])(f: A => DecodeResult[B]) = a flatMap f
+      override def map[A, B](a: DecodeResult[A])(f: A => B) = a map f
+
+      @tailrec
+      override def tailRecM[A, B](a: A)(fn : A => DecodeResult[Either[A, B]]): DecodeResult[B] =
+        fn(a).result match {
+          case Left(err)       => DecodeResult(Left(err))
+          case Right(Left(a1)) => tailRecM(a1)(fn)
+          case Right(Right(b)) => DecodeResult(Right(b))
+        }
+    }
 
   implicit def DecodeResultShow[A](implicit SE: Show[DecodeEither[A]]): Show[DecodeResult[A]] =
     SE.contramap(_.toEither)
